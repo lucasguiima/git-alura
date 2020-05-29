2dafc5a91877bf6bdc26243bf1dd35d64676d3fd
lucas.guimaraes <lucas.guimaraes@oobj.com.br>
2020-05-29 16:06:05 -0300

    Adicionado mais 2 asteristicos na mensagem a ser mostrada

--- TesteLacos2.java
+++ TesteLacos2.java
@@ -4,7 +4,7 @@ public class TesteLacos2 {
 
         for (int linha = 0; linha < 10; linha++) {
             for(int coluna = 0; coluna < linha; coluna++) {
-                System.out.print("*");
+                System.out.print("***");
             }
             System.out.println();
         }
